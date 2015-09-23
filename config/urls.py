55dd2fb15fb3ff157defb3895d7a63042f729381
mimischi <gecht.m@gmail.com>
2015-09-23 19:27:14 +0200

    New work app
    Added new work app.
    Will import data from old clock app.

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
-
+    url(r'^work/', include("clock.work.urls", namespace="work")),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
