4b6fa34da5d7b154bfcbf220dee2af7accb5d845
mimischi <gecht.m@gmail.com>
2015-10-02 10:52:39 +0200

    Only own contracts are shown
    When adding/editing a shift, the user only sees his own contracts and
    not those of other users.

--- clock/work/forms.py
+++ clock/work/forms.py
@@ -92,6 +92,9 @@ class ShiftForm(forms.ModelForm):
         # Retrieve current user, supplied by the view
         self.requested_user = self.initial['user']
 
+        self.fields['contract'].queryset = Contract.objects.filter(employee=self.requested_user)
+
+
         # Determine if we're creating a new shift or updating an existing one
         if self.initial['view'] == 'shift_create':
             add_input_text = _('Create new shift')
