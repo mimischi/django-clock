d057c661b450f459c589bd37412c7e769333659a
Michael Gecht <gecht.m@gmail.com>
2015-11-21 15:48:42 +0200

    Reset local dev database and recreated migration files. Will keep them now.

--- clock/work/migrations/0001_initial.py
+++ clock/work/migrations/0001_initial.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 from django.conf import settings
+import datetime
 
 
 class Migration(migrations.Migration):
@@ -23,5 +24,23 @@ class Migration(migrations.Migration):
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('employee', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
+        ),
+        migrations.CreateModel(
+            name='Shift',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('shift_started', models.DateTimeField(verbose_name='Shift started')),
+                ('shift_finished', models.DateTimeField(null=True, verbose_name='Shift finished')),
+                ('shift_duration', models.DurationField(null=True, verbose_name='Shift duration', blank=True)),
+                ('pause_started', models.DateTimeField(null=True, blank=True)),
+                ('pause_duration', models.DurationField(default=datetime.timedelta(0), verbose_name='Pause duration')),
+                ('note', models.TextField(verbose_name='Note', blank=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('contract', models.ForeignKey(blank=True, to='work.Contract', null=True)),
+                ('employee', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'ordering': ['-shift_started'],
+            },
         ),
     ]
