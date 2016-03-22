f9019d971d0268e1d3e799708f325a84c53e65e5
mimischi <gecht.m@gmail.com>
2016-03-23 00:21:34 +0100

    Shifts now editable. Adding new (already finished) shifts also works now. Linked shift details in all views. Updated week number output.

--- clock/work/templatetags/format_duration.py
+++ clock/work/templatetags/format_duration.py
@@ -1,4 +1,5 @@
 ﻿from django import template
+from time import strftime
 # from datetime import timedelta
 
 register = template.Library()
@@ -20,3 +21,11 @@ def format_timedelta2(td):
     # Returns the time without the microseconds
     # time = td - timedelta(microseconds=td.microseconds)
     # return time
+
+
+# Django docs says we should not use built-in |date template filter to display week numbers.
+# See: https://docs.djangoproject.com/en/1.9/ref/class-based-views/generic-date-based/#weekarchiveview
+# We will use strftime instead!
+@register.filter
+def format_week(date):
+    return date.strftime("%W")
