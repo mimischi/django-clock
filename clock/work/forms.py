0281b49b446cbaf5dc2a565212626b814e72d27e
mimischi <gecht.m@gmail.com>
2016-03-27 11:32:44 +0200

    Refactor code and adjust CSS.

--- clock/work/forms.py
+++ clock/work/forms.py
@@ -12,65 +12,6 @@ from allauth.account.forms import LoginForm
 from clock.work.models import Contract, Shift
 
 
-class ClockLoginForm(LoginForm):
-    text_input = forms.CharField()
-
-    textarea = forms.CharField(
-        widget=forms.Textarea(),
-    )
-
-    radio_buttons = forms.ChoiceField(
-        choices=(
-            ('option_one', "Option one is this and that be sure to include why it's great"),
-            ('option_two', "Option two can is something else and selecting it will deselect option one")
-        ),
-        widget=forms.RadioSelect,
-        initial='option_two',
-    )
-
-    checkboxes = forms.MultipleChoiceField(
-        choices=(
-            ('option_one', "Option one is this and that be sure to include why it's great"),
-            ('option_two', 'Option two can also be checked and included in form results'),
-            ('option_three', 'Option three can yes, you guessed it also be checked and included in form results')
-        ),
-        initial='option_one',
-        widget=forms.CheckboxSelectMultiple,
-        help_text="<strong>Note:</strong> Labels surround all the options for much larger click areas and a more usable form.",
-    )
-
-    appended_text = forms.CharField(
-        help_text="Here's more help text"
-    )
-
-    prepended_text = forms.CharField()
-
-    prepended_text_two = forms.CharField()
-
-    multicolon_select = forms.MultipleChoiceField(
-        choices=(('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')),
-    )
-
-    # Uni-form
-    helper = FormHelper()
-    helper.form_class = 'form-horizontal'
-    helper.layout = Layout(
-        Field('text_input', css_class='input-xlarge'),
-        Field('textarea', rows="3", css_class='input-xlarge'),
-        'radio_buttons',
-        Field('checkboxes', style="background: #FAFAFA; padding: 10px;"),
-        AppendedText('appended_text', '.00'),
-        PrependedText('prepended_text', '<input type="checkbox" checked="checked" value="" id="" name="">',
-                      active=True),
-        PrependedText('prepended_text_two', '@'),
-        'multicolon_select',
-        FormActions(
-            Submit('save_changes', 'Save changes', css_class="btn-primary"),
-            Submit('cancel', 'Cancel'),
-        )
-    )
-
-
 class ContractFilterform(forms.Form):
     start_date = forms.DateField()
     finish_date = forms.DateField()
@@ -233,7 +174,7 @@ class ShiftForm(forms.ModelForm):
     def clean_pause_duration(self):
         pause_duration = self.cleaned_data.get('pause_duration')
 
-        return pause_duration*60
+        return pause_duration * 60
 
     def clean(self):
         cleaned_data = super(ShiftForm, self).clean()
