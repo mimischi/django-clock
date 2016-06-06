c7bc671e7ec1015a5d05d88a8a1e52eb6d7c73a2
mimischi <gecht.m@gmail.com>
2016-06-06 19:03:32 +0200

    Resolves #66.

--- clock/exports/views.py
+++ clock/exports/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseBadRequest
 from django.utils.decorators import method_decorator
 from django.utils.translation import ugettext_lazy as _
+from django.shortcuts import get_object_or_404
 from django.views.generic.dates import MonthArchiveView
 
 from braces.views import JSONResponseMixin
@@ -54,6 +55,9 @@ class ExportNuke(PdfResponseMixin, MonthArchiveView):
         if int(context['view'].kwargs['hours']) > 80:
             raise HttpResponseBadRequest(_('We can\'t export more than 80 hours per month!'))
 
+        context['department'] = get_object_or_404(Contract, employee=self.request.user,
+                                                  pk=int(self.kwargs['pk'])).department
+
         nuke_data = {
             'INSTITUT': '',
             'NAME': '',
@@ -71,7 +75,7 @@ class ExportNuke(PdfResponseMixin, MonthArchiveView):
         total_shift_duration = timedelta(seconds=0)
         shift_list = []
         for shifts in test:
-            total_shift_duration += timedelta(seconds=shifts['working_hours']*3600)
+            total_shift_duration += timedelta(seconds=shifts['working_hours'] * 3600)
 
             shift_total_pause_duration = timedelta(seconds=0)
             if shifts['break_start'] is not None:
@@ -83,9 +87,9 @@ class ExportNuke(PdfResponseMixin, MonthArchiveView):
             shift_list.append(Shift(
                 shift_started=shifts['daystart'],
                 shift_finished=shifts['work_end'],
-                shift_duration=timedelta(seconds=shifts['working_hours']*3600),
+                shift_duration=timedelta(seconds=shifts['working_hours'] * 3600),
                 pause_duration=shift_total_pause_duration,
-                )
+            )
             )
 
         context['total_shift_duration'] = total_shift_duration
@@ -111,13 +115,13 @@ class ExportMonthClass(JSONResponseMixin, MonthArchiveView):
             context_dict = ['No shifts available for this given query.']
         else:
             context_dict = [{
-                u"employee": shift.employee.username,
-                u"contract": shift.contract_or_none,
-                u"shift_started": shift.shift_started,
-                u"shift_finished": shift.shift_finished,
-                u"pause_duration": shift.pause_duration,
-                u"shift_duration": shift.shift_duration
-            } for shift in self.object]
+                                u"employee": shift.employee.username,
+                                u"contract": shift.contract_or_none,
+                                u"shift_started": shift.shift_started,
+                                u"shift_finished": shift.shift_finished,
+                                u"pause_duration": shift.pause_duration,
+                                u"shift_duration": shift.shift_duration
+                            } for shift in self.object]
 
         return self.render_json_response(context_dict)
 
