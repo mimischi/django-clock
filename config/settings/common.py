46555731685cd83ac8e334f7ea53f4eb35f0080a
Michael Gecht <gecht.m@gmail.com>
2016-11-02 20:43:05 +0100

    Remove environ setting

--- config/settings/common.py
+++ config/settings/common.py
@@ -17,7 +17,7 @@ ROOT_DIR = environ.Path(__file__) - 3  # (/settings/config/common.py - 3 = /)
 APPS_DIR = ROOT_DIR.path('clock')
 
 env = environ.Env()
-env.read_env()
+#env.read_env()
 
 # APP CONFIGURATION
 # ------------------------------------------------------------------------------
