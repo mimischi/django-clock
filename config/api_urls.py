c40239f1dcbe1bc9551c230a7b88f470e07160e5
Michael Gecht <gecht.m@gmail.com>
2018-05-10 11:42:42 +0200

    Add first simple3

--- config/api_urls.py
+++ config/api_urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 
+app_name = 'api'
 urlpatterns = [
     path('shifts/', include('clock.shifts.api.urls')),
     path('contracts/', include('clock.contracts.api.urls'))
